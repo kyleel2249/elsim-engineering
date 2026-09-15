64060e274453ccf016dbac3aae80a21e5dabb023
Kyle El <125039072+kyleel2249@users.noreply.github.com>
2026-09-15 23:24:16 +0000

    Add robots.ts (disallow until launch)

---
+++ app/robots.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from 'next';
+
+const siteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? 'http://localhost:3000';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      disallow: '/'
+    },
+    sitemap: `${siteUrl}/sitemap.xml`
+  };
+}
